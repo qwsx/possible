b443077dba01bf566d23b69be0e5f10382189d5e
dcfvg <qwsx@dcfvg.com>
2014-02-23 20:16:32 +0100

    protect from sql injection

--- machine/tag_json.php
+++ machine/tag_json.php
@@ -1,11 +1,15 @@
 <?php
 	
 	include('_bdd.php');
+	$param = ereg_replace("[^A-Za-z0-9]", "", $_GET["term"] );
 	
-	$param = $_GET["term"];
-	$lan = $_GET["lan"];
+	switch ($_GET["lan"]) {
+	 case 'fr': $lan = "fr" break;
+   case 'fr': $lan = "en" break;
+	 default: exit();break;
+	}
 
-	$query = mysql_query("SELECT * FROM tag$lan WHERE tag REGEXP '^$param'  GROUP BY tag ORDER BY nb DESC LIMIT 0 , 6");
+	$query = mysql_query("SELECT * FROM 'tag$lan' WHERE tag REGEXP '^$param'  GROUP BY tag ORDER BY nb DESC LIMIT 0 , 6");
 	for ($x = 0, $numrows = mysql_num_rows($query); $x < $numrows; $x++) {
 		$row = mysql_fetch_assoc($query);
     
@@ -13,7 +17,5 @@
 	}
 	mysql_close($power);
 
-	echo $response = $_GET["callback"] . "(" . json_encode($tags) . ")";	
-	
-	
+	echo $response = $_GET["callback"] . "(" . json_encode($tags) . ")";
 ?>
